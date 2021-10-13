42de7d7615de808cc151a96a1713851669ca86ca
honeysonwani <honey@obvious.in>
2021-10-13 10:48:31 +0530

    When the api returns 200, download the file in the downloads folder

--- app/src/main/java/honey/download/MainActivity.kt
+++ app/src/main/java/honey/download/MainActivity.kt
@@ -2,10 +2,20 @@ package honey.download
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.widget.Button
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Retrofit
+import android.util.Log
+import android.widget.Toast
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.IOException
+import java.io.FileOutputStream
+import java.io.OutputStream
+import java.io.InputStream
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,13 +30,69 @@ class MainActivity : AppCompatActivity() {
     private fun downloadFileUsingRetrofit() {
         val downloadService = create()
         val call: Call<ResponseBody> = downloadService.downloadFile()
+
+        call.enqueue(object : Callback<ResponseBody> {
+
+            override fun onResponse(
+                call: Call<ResponseBody>,
+                response: Response<ResponseBody>
+            ) {
+                if (response.isSuccessful) {
+                    response.body()?.let { writeResponseBodyToDisk(it) }
+                    Toast
+                        .makeText(this@MainActivity, "File has been downloaded", Toast.LENGTH_LONG)
+                        .show()
+                } else
+                    Log.e("TAG", "was not successful")
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                Log.e("TAG", "error")
+            }
+        })
+    }
+
+    private fun writeResponseBodyToDisk(body: ResponseBody): Boolean {
+        return try {
+            val spreadsheet =
+                File(getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).toString() + File.separator + "spreadsheet.png")
+            Log.e("TAG", spreadsheet.toString())
+
+            var inputStream: InputStream? = null
+            var outputStream: OutputStream? = null
+            try {
+                val fileReader = ByteArray(4096)
+                val fileSize = body.contentLength()
+                var fileSizeDownloaded: Long = 0
+                inputStream = body.byteStream()
+                outputStream = FileOutputStream(spreadsheet)
+                while (true) {
+                    val read = inputStream.read(fileReader)
+                    if (read == -1) {
+                        break
+                    }
+                    outputStream.write(fileReader, 0, read)
+                    fileSizeDownloaded += read.toLong()
+                    Log.d("TAG", "file download: $fileSizeDownloaded of $fileSize")
+                }
+                outputStream.flush()
+                true
+            } catch (e: IOException) {
+                false
+            } finally {
+                inputStream?.close()
+                outputStream?.close()
+            }
+        } catch (e: IOException) {
+            false
+        }
     }
 
     private fun create(): FileDownloader {
         val retrofit =
             Retrofit
                 .Builder()
-                .baseUrl("https://api-sandbox.simple.org/api-docs#tag/analytics/")
+                .baseUrl("https://image.freepik.com/free-vector/")
                 .build()
 
         return retrofit.create(FileDownloader::class.java)
